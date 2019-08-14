650a5f873bef7da092911bf78fbf033518067ebd
Kevin Stolk <kevin@developershouse.nl>
2019-08-14 17:22:59 +0200

    remove unused code

--- src/test.ts
+++
@@ -1,8 +0,0 @@
-const Telegraf = require('telegraf')
-
-const bot = new Telegraf('813993268:AAH26cARVumTTp1Jcz27lI62wAyDEGIr-B4')
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => { ctx.reply('Hey there'); console.log(JSON.stringify(ctx)) })
-bot.launch()
